fe012e0a95a2f6aff3593af3236e4e928dca0628
Forlot Romain [IoT.bzh] <romain.forlot@iot.bzh>
2019-04-16 19:30:42 +0200

    Default USB Controller and Keyboard for aarch64
    We need the keyboard and USB Controller as well for the "qemu" libvirt type else
    an error is raised:
    "USB is disabled for this domain, but USB devices are present in the domain XML"
    
    Change-Id: I8775e3f3f1ce2a0dd7dd08a6bd792c13aba3373b
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- oz/Guest.py
+++ oz/Guest.py
@@ -475,8 +475,8 @@ class Guest(object):
         if self.tdl.arch in ["armv7l"]:
             oz.ozutil.lxml_subelement(features, "gic", attributes={'version': '2'})
         # CPU
-        if self.tdl.arch in ["aarch64", "armv7l"] and self.libvirt_type == "kvm":
-            # Possibly related to RHBZ 1171501 - need host passthrough for aarch64 and arm with kvm
+        if self.tdl.arch in ["aarch64", "armv7l"]:
+            # Possibly related to RHBZ 1171501 - need host passthrough for aarch64 and arm with kvm and qemu also
             cpu = oz.ozutil.lxml_subelement(domain, "cpu", None, {'mode': 'custom', 'match': 'exact'})
             oz.ozutil.lxml_subelement(cpu, "model", "host", {'fallback': 'allow'})
         # os
